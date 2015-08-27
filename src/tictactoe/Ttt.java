32a47dc0913fe09d41ae2d2009128847b34dc11f
Swift-path <dsfal.13@gmail.com>
2015-08-27 14:06:16 -0500

    pseudo

--- src/tictactoe/Ttt.java
+++ src/tictactoe/Ttt.java
@@ -4,7 +4,20 @@ public class Ttt {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		/** Psudo code
+		 * game board prints with 9 possible spots with promt
+		 * variables defined as x and o
+		 * players defined
+		 * game loop starts to check for a winner
+		 * game allows for player 1 to input a position for there specific charachter
+		 * inputs new character on board and prints
+		 * game allows for payer 2 to enter character
+		 * inputs new character into game board and displays
+		 * checks for winner
+		 * as long as there is no winner code repeats
+		 * 
+		 * 
+		 */
 	}
 
 }
